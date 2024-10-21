cd10283a206e07991ebb7889a28b52a445368b71
shreyask0712 <shreyask0712@gmail.com>
2024-10-21 12:12:41 +0530

    Login, Navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <p-menubar :model="items"></p-menubar>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent ({
+        setup() {
+            const items=[
+                {
+                    label: 'Accounts',
+                    items:[
+                        {label:'List Accounts', to:'/accounts'},
+                        {label: 'Create Account', to:'/accounts/create'}
+                    ]
+                },
+                {
+                    label: 'Contacts',
+                    items: [
+                        { label: 'List Contacts', to: '/contacts' },
+                        { label: 'Create Contact', to: '/contacts/create' },
+                    ],
+                },
+                {
+                    label: 'Opportunities',
+                    items: [
+                        { label: 'List Opportunities', to: '/opportunities' },
+                        { label: 'Create Opportunity', to: '/opportunities/create' },
+                    ],
+                },
+                {
+                    label: 'User',
+                    items: [
+                        { label: 'Login', to: '/login' },
+                        { label: 'Register', to: '/register' },
+                    ],
+                }
+            ];
+            return {items};
+        }
+    })
+</script>
